ff38167d622d8d59b5639a116941d9635d0646ba
권설아 <sulah0326@naver.com>
2022-05-11 23:25:46 +0900

    회원가입, 로그인, 회원정보 수정 시 아이디,비밀번호 유효성 체크

--- React/src/components/profile/Login.js
+++ React/src/components/profile/Login.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import { makeStyles } from '@mui/styles';
 import clsx from 'clsx'
 import { Link } from 'react-router-dom';
-import Box from '@mui/material/Box';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -20,6 +21,9 @@ const Login = () => {
         memberLoginid: '',
         memberPassword: '',
     });
+    const [logintTest, setLoginTest] = useState(false);   // api 붙이기 전 로그인 테스트
+    const [idCheck, setIDcheck] = useState(false);
+    const [pwCheck, setPWcheck] = useState(false);
 
     const { memberLoginid, memberPassword } = userInfo;
 
@@ -33,70 +37,58 @@ const Login = () => {
 
     const onKeyPress = (e) => {
         if (e.key == 'Enter') {
-            handleClick();
+            handleValid();
         }
     }
 
-    // const getLogin = async (userInfo) => {
-    //     try {
-    //         setLoading(true);
-    //         const response = await axios.post(API_SERVER + '/auth/signup', {
-    //             memberLoginid: userInfo.memberLoginid,
-    //             memberPassword: userInfo.memberPassword,
-    //         })
-    //         console.log('response', response)
-    //         if (response.status === 200 && response.data.access_token != null && response.data.user != null) {
-    //             console.log("유저", JSON.stringify(response.data));
-    //             // updateUserInfo(dispatch, response.data.access_token, response.data.user)
+    const getLogin = async ( userInfo ) => {
+        try {
+            setLoading(true);
+            // const response = await axios.post(API_SERVER + '/auth/signup', {
+            //     memberLoginid: userInfo.memberLoginid,
+            //     memberPassword: userInfo.memberPassword,
+            // })
+            // console.log('response', response)
+            // if (response.status === 200 && response.data.access_token != null && response.data.user != null) {
+            //     console.log("유저", JSON.stringify(response.data));
+            //     // updateUserInfo(dispatch, response.data.access_token, response.data.user)
 
-    //             Swal.fire({
-    //                 text: "로그인 되었습니다.",
-    //                 icon: "success",
-    //                 showConfirmButton: false,
-    //                 timer: 1200,
-    //             }).then(() => {
-    //                 if (location.state && location.state.referer) document.location.href = location.state.referer
-    //                 else document.location.href = '/'
-    //             });
+            //     Swal.fire({
+            //         text: "로그인 되었습니다.",
+            //         icon: "success",
+            //         showConfirmButton: false,
+            //         timer: 1200,
+            //     }).then(() => {
+            //         if (location.state && location.state.referer) document.location.href = location.state.referer
+            //         else document.location.href = '/'
+            //     });
 
-    //         }
-    //     } catch (error) {
-    //         console.log('error', error)
-    //             // Swal.fire({
-    //             //     title: "로그인에 실패하였습니다",
-    //             //     html: "아이디 또는 비밀번호가 잘못 입력 되었습니다.<br>아이디와 비밀번호를 정확히 입력해주세요.",
-    //             //     icon: "error",
-    //             //     confirmButtonText: "확인",
-    //             //     buttons: {
-    //             //         text: "확인",
-    //             //     },
-    //             // });
-    //     } 
-    //     setLoading(false);
-    // }
+            // }
+            setLoginTest(true);
+        } catch (error) {
+            console.log('error', error)
+                // Swal.fire({
+                //     title: "로그인에 실패하였습니다",
+                //     html: "아이디 또는 비밀번호가 잘못 입력 되었습니다.<br>아이디와 비밀번호를 정확히 입력해주세요.",
+                //     icon: "error",
+                //     confirmButtonText: "확인",
+                //     buttons: {
+                //         text: "확인",
+                //     },
+                // });
+        } 
+        setLoading(false);
+    }
 
-    const handleClick = () => {
+    const handleValid = (e) => {
+        e.preventDefault();
         if (!memberLoginid) {
-            // Swal.fire({
-            //     text: "아이디를 입력해주세요",
-            //     icon: "warning",
-            //     confirmButtonText: "확인",
-            //     buttons: {
-            //         cancel: "확인",
-            //     },
-            // });
+            setIDcheck(true);
         }
         else if (!memberPassword) {
-            // Swal.fire({
-            //     text: "비밀번호를 입력해주세요",
-            //     icon: "warning",
-            //     confirmButtonText: "확인",
-            //     buttons: {
-            //         cancel: "확인",
-            //     },
-            // });
+            setPWcheck(true);
         }
-        // else getLogin(userInfo);
+        else getLogin(userInfo);
     }
 
     return (
@@ -116,7 +108,7 @@ const Login = () => {
                         </div>
                     </Grid>
                     <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center'}}>
-                        <button className={clsx('btn_1', 'margin_30')}>
+                        <button className={clsx('btn_1', 'margin_30')} onClick={handleValid}>
                             로그인
                         </button>
                     </Grid>
@@ -134,6 +126,48 @@ const Login = () => {
                     </Grid>
                 </Grid>
             </form>
+            {idCheck && (
+                <div className="container">
+                    <div className="popup-wrap" > 
+                        <div className="popup">	
+                            <div className="popup-body">
+                                <div className="body-content">
+                                    <div className="body-titlebox">
+                                        <ErrorOutlineIcon style={{fontSize: '47px'}}/>
+                                    </div>
+                                    <div className="body-contentbox">
+                                        아이디를 입력하세요.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="popup-footer">
+                                <Box className="pop-btn" onClick={()=>setIDcheck(false)}>확인</Box>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {pwCheck && (
+                <div className="container">
+                    <div className="popup-wrap" > 
+                        <div className="popup">	
+                            <div className="popup-body">
+                                <div className="body-content">
+                                    <div className="body-titlebox">
+                                        <ErrorOutlineIcon style={{fontSize: '47px'}}/>
+                                    </div>
+                                    <div className="body-contentbox">
+                                        비밀번호를 입력하세요.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="popup-footer">
+                                <Box className="pop-btn" onClick={()=>setPWcheck(false)}>확인</Box>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
--- React/src/components/profile/SignUp.js
+++ React/src/components/profile/SignUp.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import clsx from 'clsx'
 import { makeStyles } from '@mui/styles';
 import { Link } from 'react-router-dom';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
@@ -20,11 +21,14 @@ const SignUp = () => {
         memberPassword: '',
         memberName: '',
     });
-    const [validCheck, setValidCheck] = useState(false);  
+    const [nameCheck, setNameCheck] = useState(false);
+    const [idCheck, setIDcheck] = useState(false);
+    const [pwCheck, setPWcheck] = useState(false);
 
     const { memberLoginid, memberPassword, memberName } = userInfo;
 
-    const isValidInput = memberName.length >= 1 && memberPassword.length >= 1 && memberLoginid.length >= 1;
+    // 유효성 체크
+    const isValidName = memberName.length >= 1;
     const isValidLoginid = memberLoginid.length >= 8;
     const specialCharacter = memberPassword.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
     const isValidPassword = memberPassword.length >= 6 && specialCharacter >= 1;
@@ -37,14 +41,15 @@ const SignUp = () => {
         });
     };
 
-    const handleValidButton = () => {
-        if ( !isValidInput ) {
-            alert('회원정보를 전부 입력해주세요');
-        } else if ( !isValidPassword ) {
-          alert('비밀번호 형식을 맞춰주세요.');
+    const handleValid = (e) => {
+        e.preventDefault();
+        if ( !isValidName ) {
+            setNameCheck(true);
         } else if( !isValidLoginid ){
-            alert('아이디 길이를 맞춰주세요.');
-        } 
+            setIDcheck(true);
+        } else if ( !isValidPassword ) {
+            setPWcheck(true);
+        }
     };
 
     return (
@@ -57,22 +62,19 @@ const SignUp = () => {
             <form className={classes.root} noValidate autoComplete="off">
                 <Grid container>
                     <Grid item xs={12} style={{ justifyContent: 'center' }}>
-                        <div style={{display:"flex", justifyContent:"space-between", margin:"20px 0"}}>
+                        <div className={clsx('between', 'margin_30')}>
                             <div className='form_name'>이름</div>
                             <div>
                                 <input type="text" placeholder="이름을 입력하세요" className='form_txt_1' value={memberName} name="memberName" onChange={handleChange} />
                             </div>
                         </div>
-                        <div style={{display:"flex", justifyContent:"space-between", margin:"20px 0"}}>
+                        <div className={clsx('between', 'margin_30')}>
                             <div className='form_name'>아이디</div>
                             <div>
                                 <input type="text" placeholder="8자 이상의 아이디를 입력하세요" className='form_txt_1' value={memberLoginid} name="memberLoginid"  onChange={handleChange}/>
                             </div>
                         </div>
-                        <div>
-                            {(validCheck && !isValidLoginid) && <p>아이디 길이가 형식에 맞지 않습니다.</p>}
-                        </div>
-                        <div style={{display:"flex", justifyContent:"space-between", margin:"20px 0"}}>
+                        <div className={clsx('between', 'margin_30')}>
                             <div className='form_name'>비밀번호</div>
                             <div>
                                 <input type="password" placeholder="영문/숫자/특수문자 포함 6자 이상 비밀번호를 입력하세요" className='form_txt_1' value={memberPassword} name="memberPassword" onChange={handleChange}/>
@@ -81,12 +83,77 @@ const SignUp = () => {
                     </Grid>
 
                     <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center'}}>
-                        <button className={clsx('btn_1', 'margin_30')} onClick={() => {setValidCheck(true);handleValidButton();}}>
+                        <button className={clsx('btn_1', 'margin_30')} onClick={handleValid}>
                             완료
                         </button>
                     </Grid>
                 </Grid>
             </form>
+            {nameCheck && (
+                <div className="container">
+                    <div className="popup-wrap" > 
+                        <div className="popup">	
+                            <div className="popup-body">
+                                <div className="body-content">
+                                    <div className="body-titlebox">
+                                        <ErrorOutlineIcon style={{fontSize: '47px'}}/>
+                                    </div>
+                                    <div className="body-contentbox">
+                                        이름을 입력하세요.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="popup-footer">
+                                <Box className="pop-btn" onClick={()=>setNameCheck(false)}>확인</Box>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {idCheck && (
+                <div className="container">
+                    <div className="popup-wrap" > 
+                        <div className="popup">	
+                            <div className="popup-body">
+                                <div className="body-content">
+                                    <div className="body-titlebox">
+                                        <ErrorOutlineIcon style={{fontSize: '47px'}}/>
+                                    </div>
+                                    <div className="body-contentbox">
+                                        입력하신 아이디가 형식에 맞지 않습니다.<br/>
+                                        8자 이상의 아이디를 입력하세요.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="popup-footer">
+                                <Box className="pop-btn" onClick={()=>setIDcheck(false)}>확인</Box>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {pwCheck && (
+                <div className="container">
+                    <div className="popup-wrap" > 
+                        <div className="popup">	
+                            <div className="popup-body">
+                                <div className="body-content">
+                                    <div className="body-titlebox">
+                                        <ErrorOutlineIcon style={{fontSize: '47px'}}/>
+                                    </div>
+                                    <div className="body-contentbox">
+                                        입력하신 비밀번호가 형식에 맞지 않습니다.<br/>
+                                        6자 이상의 영문/숫자/특수문자를 사용하세요.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="popup-footer">
+                                <Box className="pop-btn" onClick={()=>setPWcheck(false)}>확인</Box>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
