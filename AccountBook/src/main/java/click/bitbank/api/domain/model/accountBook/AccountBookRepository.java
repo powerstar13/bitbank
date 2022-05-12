92bdd038a2adc6213e2beaea0908bbe279170080
seunghoon_jeong <seunghoon_jeong@tmax.co.kr>
2022-05-12 23:15:41 +0900

    feature - 가계부 entity, repository, factory 작성
    - member domain 삭제
    - 수입, 지출, 이체 entity 작성 및 repository 추가
    - AccountBookFactory 추가 : entity별 builder 메소드 추가
    - TODO: Impl class 작성

--- AccountBook/src/main/java/click/bitbank/api/domain/model/accountBook/AccountBookRepository.java
+++
@@ -1,8 +0,0 @@
-package click.bitbank.api.domain.model.accountBook;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountBookRepository extends ReactiveCrudRepository<AccountBook, Integer> {
-}
