1a443db3cd17563b1267fa11e7264db3f6458652
홍준성 <powerstar13@hanmail.net>
2022-05-14 13:01:28 +0900

    Member, Card > Handler Test Code > BDD 적용

--- Card/src/test/java/click/bitbank/api/presentation/card/CardHandlerTest.java
+++ Card/src/test/java/click/bitbank/api/presentation/card/CardHandlerTest.java
@@ -1,43 +1,67 @@
 package click.bitbank.api.presentation.card;
 
+import click.bitbank.api.application.card.CardApplicationService;
 import click.bitbank.api.application.response.CardPopularListResponse;
-import click.bitbank.api.infrastructure.config.WebFluxRouterConfig;
-import org.junit.jupiter.api.BeforeEach;
+import click.bitbank.api.application.response.dto.CardDTO;
+import click.bitbank.api.domain.model.card.CardBenefitType;
+import click.bitbank.api.infrastructure.exception.GlobalExceptionHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
 
-@SpringBootTest
+import static click.bitbank.api.infrastructure.factory.CardTestFactory.cardPopularListResponse;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@WebFluxTest(CardHandler.class)
 class CardHandlerTest {
 
-    private WebTestClient webTestClientethod;
-
     @Autowired
-    private WebFluxRouterConfig webFluxRouterConfig;
-    @Autowired
-    private CardHandler cardHandler;
+    private WebTestClient webClient;
 
-    @BeforeEach
-    void setUp() {
-        webTestClientethod = WebTestClient
-            .bindToRouterFunction( // WebFluxConfig에서 작성한 router를 WebTestClient에 바인딩해준다.
-                webFluxRouterConfig.cardRouterBuilder(cardHandler)
-            )
-            .build();
-    }
+    @MockBean
+    private GlobalExceptionHandler globalExceptionHandler;
+    @MockBean
+    private CardApplicationService cardApplicationService;
 
     /**
      * 인기 카드 목록 조회
      */
     @Test
     void cardPopularList() {
-    
-        webTestClientethod
+        // given
+        given(cardApplicationService.findCardPopularList()).willReturn(cardPopularListResponse());
+
+        // when
+        FluxExchangeResult<CardPopularListResponse> result = webClient
             .get()
             .uri("/card/popular-list")
             .exchange()
             .expectStatus().isOk()
-            .expectBody(CardPopularListResponse.class);
+            .returnResult(CardPopularListResponse.class);
+
+        // then
+        verify(cardApplicationService).findCardPopularList();
+
+        StepVerifier.create(result.getResponseBody().log())
+            .assertNext(response -> assertAll(() -> {
+                assertEquals(HttpStatus.OK.value(), response.getRt());
+
+                CardDTO cardDTO = response.getCardDTOList().get(0);
+                assertEquals(1, cardDTO.getCardId());
+                assertEquals("카드사", cardDTO.getCardCompany());
+                assertEquals(CardBenefitType.B, cardDTO.getCardBenefitType());
+                assertEquals("혜택 내용", cardDTO.getCardBenefitContent());
+                assertEquals("이미지 경로", cardDTO.getCardImagePath());
+                assertEquals(1, cardDTO.getCardRanking());
+            }))
+            .verifyComplete();
     }
 }
