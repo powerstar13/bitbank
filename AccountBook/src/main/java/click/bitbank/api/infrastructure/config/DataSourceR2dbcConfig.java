1d288ed5f6c511ba27485a5a64ef4cd8c7f6ad2c
홍준성 <powerstar13@hanmail.net>
2022-05-14 18:57:31 +0900

    R2DBC > Auditing 연결
    1. 데이터 생성 시 > modDate 영향 없도록 처리
    2. 데이터 수정 시 > modDate 영향 있도록 처리

--- AccountBook/src/main/java/click/bitbank/api/infrastructure/config/DataSourceR2dbcConfig.java
+++ AccountBook/src/main/java/click/bitbank/api/infrastructure/config/DataSourceR2dbcConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 import org.springframework.transaction.ReactiveTransactionManager;
@@ -19,6 +20,7 @@ import java.util.Map;
 
 @Configuration
 @EnableR2dbcRepositories
+@EnableR2dbcAuditing(modifyOnCreate = false)
 public class DataSourceR2dbcConfig extends AbstractR2dbcConfiguration {
 
     @Value("${spring.r2dbc.master-url}")
