446e7c182da42ce16cf9a9ca6e903dc9adee590f
권설아 <sulah0326@naver.com>
2022-05-19 02:16:09 +0900

    1. MOBX 설정하여 상태관리 적용

---
+++ React/src/components/stores/Store.js
@@ -0,0 +1,31 @@
+import { observable, makeObservable, action } from 'mobx';
+
+const store = observable({
+    memberName : sessionStorage.getItem("memberName") || '',
+    memberType : sessionStorage.getItem("memberType") || '',
+    accessToken : sessionStorage.getItem('access_token') || '',
+    refreshToken : sessionStorage.getItem("refresh_token") || '',
+    memberId : sessionStorage.getItem('memberId') || '',
+
+  setUserInfo(data) {
+    this.memberName = data.memberName;
+    this.memberType = data.memberType;
+    this.accessToken = data.accessToken;
+    this.refreshToken = data.refreshToken;
+    this.memberId = data.memberId;
+    console.log("스토어 데이터", data)
+  },
+  setMemberName(data) {
+    this.memberName = data.memberName;
+  },
+  logOut() {
+    this.memberName = null;
+    this.memberType = null;
+    this.accessToken = null;
+    this.refreshToken = null;
+    this.memberId = null;
+  }
+});
+
+
+export { store };
---
+++ React/src/useStore.js
@@ -0,0 +1,7 @@
+import { store } from './components/stores/Store';
+
+const useStore = () => {
+  return { store };
+};
+
+export default useStore;
