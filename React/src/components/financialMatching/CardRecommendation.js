33d04793e733f2654862a77b367b6f8dbaa753e8
권설아 <sulah0326@naver.com>
2022-05-19 02:29:19 +0900

    1. 카드 추천 설문 페이지 Input에 숫자만 입력되도록 하고 숫자 입력 시 천 단위마다 자동으로 콤마(,) 찍히도록 구현 2. 소비 패턴에 따른 카드 추천 API를 연결하여 사용자의 소비 패턴에 따른 카드 추천 리스트를 받아서 화면에 제공 3. 금융매칭 버튼 영역 오류 수정

--- React/src/components/financialMatching/CardRecommendation.js
+++ React/src/components/financialMatching/CardRecommendation.js
@@ -1,44 +1,61 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { makeStyles } from '@mui/styles';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import clsx from 'clsx';
-import { Link, useHistory } from 'react-router-dom';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        fontFamily: "IBM Plex Sans KR",
-        display: "flex",
-    },
-}));
+import Loader from "./../common/Loader"
 
 
 const CardRecommendation = () => {
-    const classes = useStyles();
+    const location = useLocation();
     let [loading, setLoading] = useState(false);   
+    const [cardList, setCardList] = useState(location.state.cardList);
+
+    // 카드 혜택 유형
+    const getValue = (type) => {
+        try {
+            if( type === 'T') {
+                return "교통비 혜택"
+            } else if (type === 'P') {
+                return "통신비 혜택"
+            } else if (type === 'E') {
+                return "식사비 혜택"
+            } else if (type === 'D') {
+                return "카페비 혜택"
+            } else if (type === 'S') {
+                return "쇼핑비 혜택"
+            } else if (type === 'O') {
+                return "주유비 혜택"
+            } else if (type === 'B') {
+                return "도서비 혜택"
+            } else if (type === 'I') {
+                return "보험비 혜택"
+            } else if (type === 'M') {
+                return "대형마트비 혜택"
+            } else if (type === 'C') {
+                return "편의점 혜택"
+            } 
+        } catch (e) {
+            return '';
+        }
+    }
 
     return (
         <div>
-            {/* <Loader loading={loading} /> */}
             <div className={clsx('item_center','subtitle_2')}>나에게 가장 좋은 카드는?</div>
             <div className="subtitle_3">입력하신 소비 패턴에 맞게<br/>많은 혜택을 받을 수 있는 카드를 찾았어요!</div>
             <Grid container>
                 <Grid item xs={12} style={{ justifyContent: 'center', marginTop: '30px' }}>
-                    <div className='paper'>
-                        <div className='card_company'>삼성카드</div>
-                        <div className='card_name'>1. 삼성카드 taptap O</div>
-                        <img src="http://db.kookje.co.kr/news2000/photo/2017/0915/L20170915.99099006474i1.jpg?37" height="80" width="150"/>
-                        <div className='tag_btn'>대형마트 할인</div>
-                        <div className='card_benefit'>삼성이 혜택을 책임지고 다 드립니다. 통신비에서 10프로 할인까지!</div>
-                    </div>
-                    <div className='paper'>
-                        <div className='card_company'>KB국민카드</div>
-                        <div className='card_name'>2. KB국민 청춘대로 매니아i카드</div>
-                        <img src="https://blog.kakaocdn.net/dn/bbr8ku/btrfG14HdFy/ZOuwNuAMcTmG6KBYUOIvJ0/img.jpg" height="80" width="150"/>
-                        <div className='tag_btn'>통신비 할인</div>
-                        <div className='card_benefit'>KB국민이 혜택을 책임지고 다 드립니다. 통신비에서 10프로 할인까지!</div>
-                    </div>
-                    
+                    <Loader loading={loading} />
+                    {cardList && cardList.map((data, i) => (
+                        <div className='paper' key={data.cardRanking}>
+                            <div className='card_company'>{data.cardCompany}</div>
+                            <div className='card_name'>{i+1}&nbsp; {data.cardName}</div>
+                            <img src={data.cardImagePath} height="80" width="150"/>
+                            <div className='tag_btn'>{getValue(data.cardBenefitType)}</div>
+                            <div className='card_benefit'>{data.cardBenefitContent}</div>
+                        </div>
+                    ))}
                 </Grid>
             </Grid>
         </div>
